c39e9ec849ce416eb3343a4a96d8d77bc1869e60
Jaish Singh <jaishsingh913@gmail.com>
2023-10-22 15:26:59 +0530

    add

--- circle.py
+++ circle.py
@@ -1,2 +1,14 @@
-draw=draw("circle")
-draw()
+import turtle
+t=turtle.Turtle()
+r = 150
+r1=140
+r2=130
+r3=120
+r4=110
+r5=100
+t.circle(r)
+t.circle(r1)
+t.circle(r2)
+t.circle(r3)
+t.circle(r4)
+t.circle(r5)
